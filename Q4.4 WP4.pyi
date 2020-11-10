26cfb030e1edee600a08f2a2d93bb43aed87eafa
AdriCasab <adriano.waitze@gmail.com>
2020-11-10 16:55:35 +0100

    Creating document for 4.4 Spacing is still incorrect

---
+++ Q4.4 WP4.pyi
@@ -0,0 +1,24 @@
+#Determing the required quantity of fasteners. Main things to determine: number of fasteners and their spacing
+#Things to keep in mind: type of material -> Two types of materials. If metal 2-3; if composite 4-5.
+
+#1 indicates metal, 2 indicates composite
+w = 10
+e1 = 1
+e2 = 1
+d2 = 0.5
+material = 1
+
+def fastener_selection(w,e1,e2,d2,material):
+    fastener_count = 2
+    if material == 1:
+        fastener_spacing = 2
+    elif material == 2:
+        fastener_spacing = 4
+    else:
+        return print("Please indicate the material type")
+    usable_length = w - e1 - e2 #determining the length where the fasteners can be
+    while usable_length > (fastener_spacing*2 - d2):
+        fastener_count += 1
+        usable_length -= fastener_spacing
+    spacing = (w-e1-e2-d2)/fastener_count #Still not correct
+    return fastener_count,spacing #Returns the amount of fasteners and the spacing between the centre of fasteners
