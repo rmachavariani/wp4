b30c7efc4b4dac3d7f47a4941f0c243403526c6a
AdriCasab <adriano.waitze@gmail.com>
2020-11-12 16:10:14 +0100

    Q4.4

--- Q4.4 WP4.pyi
+++ Q4.4 WP4.pyi
@@ -1,7 +1,7 @@
 #Determing the required quantity of fasteners. Main things to determine: number of fasteners and their spacing
 #Things to keep in mind: type of material -> Two types of materials. If metal 2-3; if composite 4-5.
 
-def fastener_selection(w,e1,e2,d2,material): #Width, Edge1, Edge2, Diameter of Hole, Material Type
+def fastener_selection(w,e1,d2,material): #Width, Edge1, Diameter of Hole, Material Type
     #1 indicates metal, 2 indicates composite
     if material == 1:
         fastener_spacing = 2
@@ -9,7 +9,7 @@ def fastener_selection(w,e1,e2,d2,material): #Width, Edge1, Edge2, Diameter of H
         fastener_spacing = 4
     else:
         return print("Please indicate the material type")
-    usable_length = w - e1 - e2 #determining the length where the fasteners can be
+    usable_length = w - 2*e1 #determining the length where the fasteners can be
     fastener_count = int(((usable_length/d2)-1)/fastener_spacing)+1
-    spacing = (w-e1-e2-d2)/(fastener_count-1)
+    spacing = (w-2*e1-d2)/(fastener_count-1)
     return fastener_count,spacing #number of fastener and spacing between them
