1e03705c92d37611fdf3845a7805c278d469c8e7
Mauro <maubeend@gmail.com>
2020-11-24 15:11:54 +0100

    Corrected PullThroughCheck.py

--- PullThroughCheck.py
+++ PullThroughCheck.py
@@ -1,7 +1,6 @@
 from math import pi, sqrt
 from numba import njit
 import numpy as np
-import time
 
 
 @njit()
@@ -98,75 +97,75 @@ def pull_through(lug, fastener, fastener_grid, forces, moments, material):
     """
 
     n_f = fastener[2]  # Number of fasteners
-    D_fi = fastener[1]  # Inner diameter of the fastener
-    D_fo = fastener[0]  # Outer diameter of the fastener
-    F_y = forces[3][0]  # Tensile Force
+    d_fi = fastener[1]  # Inner diameter of the fastener
+    d_fo = fastener[0]  # Outer diameter of the fastener
+    f_y = forces[3][0]  # Tensile Force
     t2 = lug[4]  # Thickness of the plate
     t3 = lug[5]  # Thickness of the vehicle wall
-    M_z = moments[3][1]  # Moment of the solar panel
-    yieldstress_backplate = material[0]  # Yield stress of the plates
-    yieldstress_vehicleplate = material[1]
-    listcoordinates = fastener_grid
+    m_z = moments[3][1]  # Moment of the solar panel
+    yield_stress_back_plate = material[0]  # Yield stress of the plates
+    yield_stress_vehicle_plate = material[1]
+    list_coordinates = fastener_grid
 
     # Determining the shear yield stress
-    shearyieldstress_backplate = yieldstress_backplate / sqrt(3)
-    shearyieldstress_vehicleplate = yieldstress_vehicleplate / sqrt(3)
+    shear_yield_stress_back_plate = yield_stress_back_plate / sqrt(3)
+    shear_yield_stress_vehicle_plate = yield_stress_vehicle_plate / sqrt(3)
 
     # Areas
-    A_shear = pi * D_fo * (t2 + t3)
-    A_tension = (1 / 4) * pi * (D_fi ** 2)
+    a_shear = pi * d_fo * (t2 + t3)
+    a_tension = (1 / 4) * pi * (d_fi ** 2)
 
     # Lists
     distances = []
-    margin_backplate = []
-    margin_vehicleplate = []
+    margin_back_plate = []
+    margin_vehicle_plate = []
 
     # Distance between fastener and cg
-    for radius in range(0, len(listcoordinates)):
-        x_coord = listcoordinates[radius][0]
-        z_coord = listcoordinates[radius][1]
+    for radius in range(0, len(list_coordinates)):
+        x_coord = list_coordinates[radius][0]
+        z_coord = list_coordinates[radius][1]
 
         pythagoras = sqrt((x_coord ** 2) + (z_coord ** 2))
         distances.append(pythagoras)
 
     # Summation of the area multiplied by the distance
-    summation = A_tension * sum(distances)
+    summation = a_tension * sum(distances)
 
     # Force in the y-direction on each fastener
-    F_pi = F_y / n_f
+    force_pi = f_y / n_f
 
     # Calculating the shear stress on the fastener and the sheets
     n = 0
     for i in distances:
 
         # Forces on a fastener
-        F_pMz = (-M_z * i * A_tension) / summation
+        force_pmz = (-m_z * i * a_tension) / summation
 
         # Total Force and shear stress
-        if (listcoordinates[n][0] > 0):
-            F_T = F_pi - F_pMz
-            shearstress = F_T / A_shear
+        if list_coordinates[n][0] > 0:
+            force_t = force_pi - force_pmz
+            shear_stress = force_t / a_shear
         else:
-            F_T = F_pi + F_pMz
-            shearstress = F_T / A_shear
+            force_t = force_pi + force_pmz
+            shear_stress = force_t / a_shear
         n = n + 1
 
-        difference_backplate = shearstress - shearyieldstress_backplate
-        difference_vehicleplate = shearstress - shearyieldstress_vehicleplate
+        difference_back_plate = shear_stress - shear_yield_stress_back_plate
+        difference_vehicle_plate = shear_stress - shear_yield_stress_vehicle_plate
 
-        margin_backplate.append(difference_backplate)
-        margin_vehicleplate.append(difference_vehicleplate)
+        margin_back_plate.append(difference_back_plate)
+        margin_vehicle_plate.append(difference_vehicle_plate)
 
     # Easy check to see if the structure will fail
-    print('Pull Through check for the backplate')
-    for j in margin_backplate:
+    print('Pull Through check for the back plate')
+    for j in margin_back_plate:
         if j >= 0:
             print('This fastener fails!!!!!!!!!!!!!!!!!')
         else:
             print('This configuration is fine')
 
-    print('Pull through check for the vehicleplate')
-    for k in margin_vehicleplate:
+    print('Pull through check for the vehicle plate')
+    for k in margin_vehicle_plate:
         if k >= 0:
             print('This fastener fails!!!!!!!!!!!!!!!!!')
         else:
@@ -177,4 +176,4 @@ def pull_through(lug, fastener, fastener_grid, forces, moments, material):
     If the value is positive, if the margin is positive, then pull through occurs.
     """
 
-    return margin_vehicleplate, margin_backplate
+    return margin_vehicle_plate, margin_back_plate
