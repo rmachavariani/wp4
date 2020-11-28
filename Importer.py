4530d540b03e34f34017db8885af3e62e353f646
Mauro <maubeend@gmail.com>
2020-11-28 13:07:28 +0100

    added indices to material importer.py

--- Importer.py
+++ Importer.py
@@ -10,7 +10,8 @@ class Material:
         self.name = ""
         self.type = ""
         self.code = ""
-        self.index = ""
+        self.index_12 = ""
+        self.index_15 = ""
         self.density = 0
         self.e = 0
         self.e_0 = 0
@@ -24,6 +25,7 @@ class Material:
 
     def import_material(self, material_type, material_choice):
         self.material_type = material_type
+        print(f"Importing {material_type}, {material_choice}")
         try:
             with open(f"./{self.path}.csv") as file:
                 reader = csv.reader(file, delimiter=',')
@@ -32,14 +34,13 @@ class Material:
                     if f'variable_name_{material_type}' in row[0]:
                         component_attributes = row
 
-                    if (row[1].strip().lower(), row[2].strip().lower()) == (material_choice[0], material_choice[1].lower()):
+                    if (row[1].strip().lower(), row[2].strip().lower()) == (material_choice[0].strip().lower(), material_choice[1].strip().lower()):
                         for i, value in enumerate(row):
                             self.__dict__[component_attributes[i].strip().lower()] = value
 
         except FileNotFoundError:
-            print("UI components file not found, make sure that 'variable_name_[metal/composite]' at start of line")
+            print("Material sheet file not found, make sure that the right path is given")
             raise SystemExit
-        print(component_attributes)
 
 
 def import_all_materials(path, material_types):
@@ -57,25 +58,48 @@ def import_all_materials(path, material_types):
 
                     material_name = ""
                     material_main_type = ""
-                    for i, cell in enumerate(row):
-                        if component_attributes[i] == "name":
-                            material_name = str(cell)
+                    index_12 = False
+                    index_15 = False
+                    if material_type.strip().lower() == str(row[0]).strip().lower():
+                        try:
+                            for i, cell in enumerate(row):
+                                if component_attributes[i] == "name":
+                                    material_name = str(cell)
 
-                        if component_attributes[i] == "type":
-                            material_main_type = str(cell)
+                                if component_attributes[i] == "type":
+                                    material_main_type = str(cell)
 
-                        if material_name != "" and material_main_type != "":
-                            material_iteration = Material(path)
-                            material_iteration.import_material(str(material_type), (material_name, material_main_type))
-                            material_list.append(material_iteration)
+                                if component_attributes[i] == "index_12":
+                                    if cell != "-":
+                                        index_12 = True
+
+                                if component_attributes[i] == "index_12":
+                                    if cell != "-":
+                                        index_15 = True
+
+                                if material_name != "" and material_main_type != "" and index_12 and index_15:
+                                    material_iteration = Material(path)
+                                    material_iteration.import_material(str(material_type), (material_name, material_main_type))
+                                    material_list.append(material_iteration)
+                                    break
+
+                                if i == len(row) - 1:
+                                    if not index_12 or not index_15:
+                                        print(f"{material_name} was not appended: index_12 = {index_12}, index_15 = {index_15}")
+                                    else:
+                                        print(f"{material_name} was not appended as it does not have a type")
+
+                        except IndexError:
+                            print("Error in reading materials file")
+                            raise SystemExit
 
     except FileNotFoundError:
-        print("UI components file not found, make sure that 'variable_name_[metal/composite]' at start of line")
+        print("Material sheet file not found, make sure that the right path is given")
         raise SystemExit
-    print(component_attributes)
 
+    return material_list
 
-material = Material("material_sheet")
-material.import_material("metal", ("aluminium", "7075-T6"))
-print(material.density)
-
+# Test
+# materials = import_all_materials("material_sheet", ("metal", "none"))
+# for material in materials:
+#     print(material.name, material.type)
