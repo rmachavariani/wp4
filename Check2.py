93fbbc5005cd7fc39fbfc2e224ded8a46eb9b9d4
rusudanmachavariani <rusudan@elnino.tech>
2020-11-29 20:05:01 +0100

    finish check 2

--- Check2.py
+++ Check2.py
@@ -2,36 +2,36 @@ import math as m
 
 import Graphs
 
-
 # e/D equal to 1 - assumption
 
-def check_shear_bearing(hole_diameter, lug_thickness, edge_to_center_hole_distance, lug_allowable_stress,
-                        material_index, material_tensile_strength, lug_yield_strength, compressive_yield_strength_bushing):
+def check_shear_bearing( hole_diameter, lug_thickness, edge_to_center_hole_distance,
+                        material_index, material_tensile_strength, ultimate_tensile_strength, ultimate_compressive_strength):
 
     # 1 - shear-bearing failure
     margin_of_safety = 0.15
-    material_tensile_strength = material_tensile_strength / margin_of_safety
+    ultimate_tensile_strength = ultimate_tensile_strength / margin_of_safety
     x_input = edge_to_center_hole_distance / hole_diameter
     K_bru = Graphs.graph(13, material_index, x_input)
     A_br = hole_diameter * lug_thickness
 
-    P_bru = K_bru * material_tensile_strength * A_br
+    P_bru = K_bru * ultimate_tensile_strength * A_br
 
     # 2 - shear-bearing yield failure
     K_bry = Graphs.graph(14, material_index, x_input)
-    P_bry = K_bry * material_tensile_strength * A_br
+    P_bry = K_bry * ultimate_tensile_strength * A_br
 
     # 3 - bushing yield
-    P_bush_y = 1.85 * compressive_yield_strength_bushing * A_br
+    P_bush_y = 1.85 * ultimate_compressive_strength * A_br
 
-    shear_bearing_margin = lug_allowable_stress - P_bru
-    shear_bearing_yield_margin = lug_yield_strength - P_bry
+    shear_bearing_margin = P_bru - material_tensile_strength
+    shear_bearing_yield_margin = P_bry - material_yield_strength
+    bushing_margin = P_bush_y - material_yield_strength
 
-    return P_bru, P_bry, P_bush_y, shear_bearing_margin, shear_bearing_yield_margin
+    return P_bru, P_bry, P_bush_y, shear_bearing_margin, shear_bearing_yield_margin, bushing_margin
 
 
 def check_bolt_pin_bending(hole_diameter, lug_thickness, lug_height, edge_to_center_hole_distance,
-                           lug_allowable_stress, material_index, material_tensile_strength, lug_yield_strength,
+                            material_index, material_tensile_strength, lug_yield_strength,
                            axial_load, transverse_load, ultimate_allowable_tension_load,
                            ultimate_shear_bearing_failure):
 
@@ -47,10 +47,14 @@ def check_bolt_pin_bending(hole_diameter, lug_thickness, lug_height, edge_to_cen
     K_tu = Graphs.graph(15, material_index, x_input)
     K_ty = Graphs.graph(15, material_index, x_input)
 
-    P_tu = K_tu * A_br * material_tensile_strength
-    P_ty = K_ty * A_br * lug_yield_strength
-
     R_a = axial_load / min(ultimate_allowable_tension_load, ultimate_shear_bearing_failure)
-    R_tr = transverse_load / P_tu
+    R_tr = pow((1 - pow(R_a, 1.6)), 0.8)
     margin_of_safety = 1 / pow((pow(R_a, 1.6) + pow(R_tr, 1.6)), 0.625) - 1
 
+    P_tu_transverse = margin_of_safety * K_tu * A_br * transverse_load
+    P_ty_transverse = margin_of_safety * K_ty * A_br * transverse_load
+
+    ultimate_transverse_margin = material_tensile_strength - P_tu_transverse
+    ultimate_transverse_yield_margin = material_yield_strength - P_ty_transverse
+
+    return P_tu_transverse, P_ty_transverse, ultimate_transverse_margin, ultimate_transverse_yield_margin
